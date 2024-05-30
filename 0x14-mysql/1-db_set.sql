dd3850651613027c6f12086825e21c0fe4d75488
Smolclin <144316893+Smolclin@users.noreply.github.com>
2024-05-30 08:05:17 +0300

    Delete 0x14-mysql directory

--- 0x14-mysql/1-db_set.sql
+++
@@ -1,4 +0,0 @@
--- Create a user with replication client privilege
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
