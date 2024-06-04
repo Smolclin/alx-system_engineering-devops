ff3e99f816257dde9a8133a15452a355b8f4c15b
Smolclin <clinsmol10@gmail.com>
2024-06-04 12:33:37 -0700

    Fix code error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,14 +11,14 @@ if __name__ == '__main__':
     name_url = main_url + "/user/{}".format(argv[1])
     todo_result = get(todo_url).json()
     name_result = get(name_url).json()
-    
+
     todo_num = len(todo_result)
     todo_complete = len([todo for todo in todo_result
                          if todo.get("completed")])
-    
+
     name = name_result.get("name")
     print("Employee {} is done with tasks({}/{}):"
-              .format(name, todo_complete, todo_num))
+          .format(name, todo_complete, todo_num))
     for todo in todo_result:
         if (todo.get("completed")):
             print("\t {}".format(todo.get('title')))
