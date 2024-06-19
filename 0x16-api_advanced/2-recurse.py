f13f8e8c00f4bd3a8a892b070869b50dce6c647d
Smolclin <clinsmol10@gmail.com>
2024-06-19 09:33:35 -0700

    Fix code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,27 +5,22 @@ a list containing the titles of all hot articles for
 a given subreddit. If no results are found for the
 given subreddit, the function should return """
 
+import requests
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ function that queries the Reddit API """
-    import requests
 
-    info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                        .format(subreddit),
-                        params={"count": count, "after": after},
-                        headers={"User-Agent": "My-User-Agent"},
-                        allow_redirects=False)
-    if info.status_code >= 400:
+def recurse(subreddit, hot_list=[], n=0, after=None):
+    """ The Reddit API uses pagination
+    for separating pages of responses """
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': 'custom'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        r = r.json()
+        for post in r.get('data').get('children'):
+            hot_list.append(post.get('data').get('title'))
+        if r.get('data').get('after'):
+            recurse(subreddit, host_list)
+        return hot_list
+    else:
         return None
-
-    hot_1 = hot_list + [child.get("data").get("title")
-                        for child in info.json()
-                        .get("data")
-                        .get("children")]
-
-    inf = info.json()
-    if not inf.get("data").get("after"):
-        return hot_1
-
-    return recurse(subreddit, hot_1, inf.get("data").get("count"),
-                   inf.get("data").get("after"))
