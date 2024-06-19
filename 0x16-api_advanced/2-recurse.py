56112f8853b7bcdf2bb74f83d562a1de9f722635
Smolclin <clinsmol10@gmail.com>
2024-06-19 09:40:02 -0700

    Fix code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ given subreddit, the function should return """
 import requests
 
 
-def recurse(subreddit, hot_list=[], n=0, after=None):
+def recurse(subreddit, hot_list=[]):
     """ The Reddit API uses pagination
     for separating pages of responses """
 
