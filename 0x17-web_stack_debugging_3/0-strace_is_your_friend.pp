811f62312311d4e31bd2c2c7ebaf17b9c0b16ca6
Smolclin <clinsmol10@gmail.com>
2024-06-23 12:00:59 -0700

    Fix issue

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix Apache returning a 500 error
 
 exec {'fix error':
-  command => 'sed -i s/phpp/php/g /var/www/htmlwp-settings.php',
-  path    => ['/bin', '/usr/bin/', '/usr/local/bin/']
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
